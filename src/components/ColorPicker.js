a91c632e64dc77f070d1016028661965c3f7a0b6
Cara Ponzini <ponzini@gmail.com>
2019-05-14 13:31:14 -0700

    test passed for display, updating picker

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,27 +1,22 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
 // import styles from './colorPicker.css';
 
-export default class ColorPicker extends PureComponent {
-  state = {
-    color: ''
-  };
+function ColorPicker({ selectColorHandler }){
 
-  handleClick = event => {
-    this.setState({ color: event.target.textContent });
-  };
-
-  render() {
-    let color = this.state.color;
-
-    return (
-      <section>
-        <h1>Pick Colors!</h1>
-        <button style={{ background: 'purple' }}onClick={this.handleClick}>Purple</button>
-        <button style={{ background: 'blue' }}onClick={this.handleClick}>Blue</button>
-        <button style={{ background: 'green' }}onClick={this.handleClick}>Green</button>
-        <div style={{ background: color, height: '100px', width: '100px', display: 'in-line-block' }}>CHANGE MY COLOR!</div>
-      </section>
-
-    );
-  }
+  return (
+    <section>
+      <h1>Pick Colors!</h1>
+      <button style={{ background: 'purple' }}onClick={() => selectColorHandler('purple')}>Purple</button>
+      <button style={{ background: 'blue' }}onClick={() => selectColorHandler('blue')}>Blue</button>
+      <button style={{ background: 'green' }}onClick={() => selectColorHandler('green')}>Green</button>
+    </section>
+  );
 }
+
+ColorPicker.propTypes = {
+  selectColorHandler: PropTypes.func.isRequired
+};
+
+export default ColorPicker;
