0b1d7c8bc0b1490b7ef693bd8774384a04e259c4
Cara Ponzini <ponzini@gmail.com>
2019-05-14 11:32:57 -0700

    color picker works

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,20 +1,27 @@
 import React, { PureComponent } from 'react';
+// import styles from './colorPicker.css';
 
 export default class ColorPicker extends PureComponent {
   state = {
     color: ''
   };
 
+  handleClick = event => {
+    this.setState({ color: event.target.textContent }, () => {
+      console.log(this.state.color);
+    });
+  };
+
   render() {
-    let color = this.state;
-    
+    let color = this.state.color;
+
     return (
       <section>
         <h1>Pick Colors!</h1>
-        <button>Purple</button>
-        <button>Blue</button>
-        <button>Green</button>
-        <span></span>
+        <button style={{ background: 'purple' }}onClick={this.handleClick}>Purple</button>
+        <button style={{ background: 'blue' }}onClick={this.handleClick}>Blue</button>
+        <button style={{ background: 'green' }}onClick={this.handleClick}>Green</button>
+        <span style={{ background: color, height: '100px', width: '100px', display: 'in-line-block' }}>block</span>
       </section>
 
     );
--- src/components/ColorPicker.test.js
+++ src/components/ColorPicker.test.js
@@ -7,4 +7,13 @@ describe('color picker', () => {
     const wrapper = shallow(<ColorPicker />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('changes color to purple on click', () => {
+    const wrapper = shallow(<ColorPicker />);
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state('color')).toEqual('Purple');
+  });
+
+
 });
